212528877140e0005a4c02e85833efb441ae4a04
lpwlx <mgorelkov@gmail.com>
2023-12-04 03:33:26 +0100

    i don't even know how to name commit

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -21,54 +21,108 @@ namespace Calculator {
 	/// Логика взаимодействия для MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
+		Button pressedButton = null;
 		public MainWindow() {
 			InitializeComponent();
 			foreach (UIElement el in ButtonGrid.Children) {
 				if (el is Button) {
-					((Button)el).Click += ButtonClick;
+					switch (((Button)el).Name)
+					{
+						case "erase":
+							((Button)el).Click += EraseButtonClick;
+							break;
+						case "clear":
+							((Button)el).Click += ClearButtonClick;
+							break;
+						case "copy":
+							((Button)el).Click += CopyButtonClick;
+							break;
+						case "equals":
+							((Button)el).Click += EqualsButtonClick;
+							break;
+						default:
+							((Button)el).Click += ButtonClick;
+							break;
+					}
 				}
 			}
+			inOut.Focus();
 		}
 
-		private void Calculate() {
-			string pattern = @"√(\d+)"; // поиск символов корня и их следующих цифр
-			Regex regex = new Regex(pattern);
-			string processedInput = regex.Replace(inOut.Text, "√($1)");
-			try {
-				NCalc.Expression expression = new NCalc.Expression(processedInput.Replace(',', '.').Replace("√", "Sqrt"));
-				object result = expression.Evaluate();
+		private void CopyButtonClick(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException(); //---------------
+		}
 
-				if (result != null) {
-					inOut.Text = result.ToString();
-				}
-				else {
-					inOut.Text = "Error";
-				}
+		private void ClearButtonClick(object sender, RoutedEventArgs e)
+		{
+			HandleClearButtonClick();
+		}
+
+		private void HandleClearButtonClick()
+		{
+			throw new NotImplementedException(); //-----------
+		}
+
+		private void EraseButtonClick(object sender, RoutedEventArgs e)
+		{
+			HandleEraseButtonClick();
+		}
+
+		private void HandleEraseButtonClick()
+		{
+			pressedButton = erase;
+			int caretIndex = inOut.CaretIndex;
+			if(caretIndex > 0)
+			{
+				inOut.Text = inOut.Text.Remove(caretIndex - 1, 1);
+				inOut.CaretIndex = caretIndex - 1;
 			}
-			catch (EvaluationException ex) {
-				inOut.Text = "Error: " + ex.Message;
-			}
+			inOut.Focus();
+		}
+
+		private void EqualsButtonClick(object sender, RoutedEventArgs e)
+		{
+			HandleEqualsButtonClick();
+		}
+
+		private void HandleEqualsButtonClick()
+		{
+			pressedButton = equals;
+			//-------------------
 		}
 
 		private void ButtonClick(object sender, RoutedEventArgs e) {
+			/*
 			//string str = e.OriginalSource as string;
 			string str = (string)((Button)e.OriginalSource).Content;
 			int caretIndex = inOut.CaretIndex;
 			inOut.Text = inOut.Text.Insert(inOut.CaretIndex, str); // вставка по позиции курсора
 			inOut.CaretIndex = caretIndex + 1;
+			inOut.Focus(); 
+			*/
+			HandleButtonClick(sender as Button);
+		}
+		private void HandleButtonClick(Button button) {
+			pressedButton = button;
+			int caretIndex = inOut.CaretIndex;
+			inOut.Text = inOut.Text.Insert(inOut.CaretIndex, (string)button.Content); // вставка по позиции курсора
+			inOut.CaretIndex = caretIndex + 1;
 			inOut.Focus();
 		}
 
-		private void InOutTextBox_PreviewKeyDown(object sender, KeyEventArgs e) {
+		private void InOut_PreviewKeyDown(object sender, KeyEventArgs e) {
 			switch (e.Key) {
 				case Key.Left:
-					if (inOut.CaretIndex > 0)
+					if (inOut.CaretIndex > 0) {
 						inOut.CaretIndex--;
+					}
 					e.Handled = true;
 					break;
 				case Key.Right:
-					if (inOut.CaretIndex < inOut.Text.Length)
+					if (inOut.CaretIndex < inOut.Text.Length) {
 						inOut.CaretIndex++;
+					}
 					e.Handled = true;
 					break;
 				case Key.Space:
@@ -76,13 +130,71 @@ namespace Calculator {
 					break;
 				case Key.Enter:
 					e.Handled = true;
+					HandleEqualsButtonClick();
 					Calculate();
 					inOut.CaretIndex = inOut.Text.Length;
+					break;
+				case Key.Back:
+					e.Handled = true;
+					HandleEraseButtonClick();
+					break;
+				case Key.OemComma:
+					e.Handled = true;
+					HandleButtonClick(point);
+					break;
+				default:
+					e.Handled = true;
+					char sym = (char)e.Key;
+					Button button = null;
+					bool flag = false;
+					foreach (UIElement el in ButtonGrid.Children) {
+						if (el is Button && ((Button)el).Content != null) {
+							if (sym == ((string)((Button)el).Content)[0]) {
+								button = (Button)el;
+								flag = true;
+							}
+							else {
+								switch (sym) {
+									case ',':
+										button = point;
+										flag = true;
+										break;
+									case '*':
+										button = multiply;
+										flag = true;
+										break;
+									case '/':
+										button = divide;
+										flag = true;
+										break;
+									case '-':
+										button = minus;
+										flag = true;
+										break;
+									/*
+									case '\r':
+										button = erase;
+										flag = true;
+										break;
+									case '\u007F':
+										button = clear;
+										flag = true;
+										break;
+									*/
+								}
+							}
+						}
+						if (flag) break;
+					}
+					if(button != null) {
+						HandleButtonClick(button);
+						SetButtonPressedState(button, true);
+					}
 					break;
 			}
 		}
 
-		private void InOutTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e) {
+		private void InOut_PreviewTextInput(object sender, TextCompositionEventArgs e) {
 			e.Handled = true; // блокировка ввода текста
 			/*
 			if (Char.IsDigit(e.Text[0]) || e.Text == "+" || e.Text == "-" || e.Text == "*" || e.Text == "/" || e.Text == "^" || e.Text == "." || e.Text == "," || e.Text == "(" || e.Text == ")") {
@@ -91,6 +203,7 @@ namespace Calculator {
 				inOut.CaretIndex = caretIndex + 1;
 			}
 			*/
+			/*
 			Button button = null;
 			foreach (UIElement el in ButtonGrid.Children) {
 				if (el is Button) {
@@ -116,31 +229,42 @@ namespace Calculator {
 								break;
 						}
 					}
+					//button.SetValue(Button.IsPressedProperty, isPressed);
 				}
+			}
+			*/
+		}
+		private void InOut_PreviewKeyUp(object sender, KeyEventArgs e) {
+			SetButtonPressedState(pressedButton, false);
+		}
+		private void SetButtonPressedState(Button button, bool isPressed) {
+			if (button != null) {
+				//button.SetValue(Button.IsPressedProperty, isPressed);
+			}
+		}
+		private void Calculate()
+		{
+			string pattern = @"√(\d+)"; // поиск символов корня и их следующих цифр
+			Regex regex = new Regex(pattern);
+			string processedInput = regex.Replace(inOut.Text, "√($1)");
+			try
+			{
+				NCalc.Expression expression = new NCalc.Expression(processedInput.Replace('–', '-')/*.Replace(',', '.')*/.Replace("√", "Sqrt"));
+				object result = expression.Evaluate();
+
+				if (result != null)
+				{
+					inOut.Text = result.ToString();
+				}
+				else
+				{
+					inOut.Text = "Error";
+				}
+			}
+			catch (EvaluationException ex)
+			{
+				inOut.Text = "Error: " + ex.Message;
 			}
 		}
 	}
-
-
-	/*
-	private void InOut_PreviewKeyDown(object sender, KeyEventArgs e) {
-		TextBox textBox = (TextBox)sender;
-
-		switch (e.Key) {
-			case Key.Left:
-				if (textBox.CaretIndex > 0) {
-					textBox.CaretIndex--;
-				}
-				e.Handled = true;
-				break;
-			case Key.Right:
-				if (textBox.CaretIndex < textBox.Text.Length) {
-					textBox.CaretIndex++;
-				}
-				e.Handled = true;
-				break;
-				// Другие обработки клавиш, если необходимо
-		}
-	}
-	*/
 }
